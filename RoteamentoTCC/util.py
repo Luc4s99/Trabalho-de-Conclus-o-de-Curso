a83711e9b09912687dc31c0d44701ba87c3569d5
Luc4s99 <luacsm.99@gmail.com>
2021-08-27 23:37:10 -0300

    Retirando florestas do mapa e excluindo nós remanescentes

--- RoteamentoTCC/util.py
+++ RoteamentoTCC/util.py
@@ -43,13 +43,15 @@ def le_arquivo(arquivo_entrada: str):
     arvore = ET.parse(arquivo_entrada)
     # Obtém a tag raiz do arquivo
     raiz = arvore.getroot()
+    # Referencias de nós de caminhos para serem removidos
+    referencias_nos = []
 
     # Lista de itens que serão removidos, esses itens não são interessantes para o trabalho
     # Ex: Rios, ferrovias, montanhas, morros, sinais de trânsito, estabelecimentos
     remover = ['traffic_signals', 'place_of_worship', 'church', 'supermarket', 'educational_institution', 'school',
                'hospital', 'atm', 'bakery', 'bank', 'pharmacy', 'bus_station', 'hotel', 'convenience', 'taxi',
                'restaurant', 'library', 'police', 'furniture', 'sports_centre', 'tower', 'fast_food', 'peak', 'river',
-               'rail']
+               'rail', 'wood']
 
     # Armazena as tags que serão retiradas
     limpar = []
@@ -98,16 +100,11 @@ def le_arquivo(arquivo_entrada: str):
             # Guardando o id da rua
             id_rua = ramo.get('id')
 
-            # Instancia uma rua
-            rua = Rua()
-
             # Identifica se aquela tag será retirada
             isremovida = False
 
-            # Obtém o id da rua, latitude e longitude
-            rua.set_id(id_rua)
-            rua.set_latitude(ramo.get('lat'))
-            rua.set_longitude((ramo.get('lon')))
+            # Lista auxiliar para nós dos caminhos
+            aux_ref = []
 
             # Percorre todas as tags filhas da tag ramo
             for filha_ramo in ramo:
@@ -115,15 +112,8 @@ def le_arquivo(arquivo_entrada: str):
                 # Tag que possui as informações dos nós que formam a rua
                 if filha_ramo.tag == 'nd':
 
-                    # Pega a referencia do nó
-                    ref = filha_ramo.get('ref')
-
-                    # Verifica se o nó existe na lista de nós
-                    if ref in pontos.keys():
-
-                        # Insere na lista de nós da rua
-                        # O ponto é buscado na lista de pontos com base na chave, que é seu id
-                        rua.insere_ponto(pontos[ref])
+                    # Insere o nó na lista auxiliar de referências daquele caminho
+                    aux_ref.append(filha_ramo.get('ref'))
 
                 # Se alguma filha possuir a tag 'tag'
                 elif filha_ramo.tag == 'tag':
@@ -140,6 +130,10 @@ def le_arquivo(arquivo_entrada: str):
             # Se a tag estiver marcada, ela é colocada na lista de limpeza
             if isremovida:
 
+                # Nós da lista de referencia são passados para a lista final
+                for no in aux_ref:
+                    referencias_nos.append(no)
+
                 # Se o ramo já não estiver na lista de limpeza, ele é adicionado
                 if ramo not in limpar:
                     limpar.append(ramo)
@@ -147,17 +141,50 @@ def le_arquivo(arquivo_entrada: str):
             # Senão a rua é inserida no dicionário de ruas
             else:
 
+                # Instancia uma rua
+                rua = Rua()
+
+                # Seta o id da rua
+                rua.set_id(id_rua)
+
+                for ref in referencias_nos:
+
+                    # Verifica se o nó existe na lista de nós
+                    if ref in pontos.keys():
+
+                        # Insere na lista de nós da rua
+                        # O ponto é buscado na lista de pontos com base na chave, que é seu id
+                        rua.insere_ponto(pontos[ref])
+
                 # Define a chave(id da rua) e insere no dicionário
                 ruas[id_rua] = rua
+
+    # Percorrendo o arquivo novamente retirando nós remanescentes
+    for ramo in raiz:
+
+        # Limpa as tags de caminhos que não serão utilizados
+        if ramo.tag == 'node':
+
+            # Se o id desse nó estiver nos nós remanescentes, ele será excluído
+            if ramo.get('id') in referencias_nos:
+
+                limpar.append(ramo)
+
+                # Retira dos pontos mapeados os nós de caminhos excluídos
+                if pontos.__contains__(ramo.get('id')):
+                    pontos.pop(ramo.get('id'))
+
+        if ramo.tag == 'way':
+            break
 
     # Retira as tags do documento
     for item in limpar:
         raiz.remove(item)
 
-    print("Arquivo de saída gerado!")
-
     # Cria um documento de saída mais enxuto, sem tags que não serão utilizadas
     arvore.write('saida.osm')
+
+    print("Arquivo de saída gerado!")
 
 
 # Função para plotagem do mapa obtido no google maps, para confirmação do posicionamento de ruas
